f6e599b21dfd8712f76b8594c33b262e0b338851
aixxe <me@aixxe.net>
2023-11-27 23:13:11 +0000

    Enable compile-time checks in log methods

--- src/avs2-log.h
+++ src/avs2-log.h
@@ -2,21 +2,24 @@
 
 #include <format>
 
-extern "C" void log_body_fatal(const char* module, const char* fmt, ...);
-extern "C" void log_body_info(const char* module, const char* fmt, ...);
-extern "C" void log_body_misc(const char* module, const char* fmt, ...);
-extern "C" void log_body_warning(const char* module, const char* fmt, ...);
+extern "C"
+{
+    void log_body_fatal(const char* module, const char* fmt, ...);
+    void log_body_info(const char* module, const char* fmt, ...);
+    void log_body_misc(const char* module, const char* fmt, ...);
+    void log_body_warning(const char* module, const char* fmt, ...);
+}
 
 namespace avs2::log
 {
     auto constexpr module = "2dxcamhook";
 
-    template <typename... Args> auto fatal(std::string_view fmt, Args&&... args)
-        { log_body_fatal(module, "%s", std::vformat(fmt, std::make_format_args(std::forward<Args>(args)...)).c_str()); }
-    template <typename... Args> auto info(std::string_view fmt, Args&&... args)
-        { log_body_info(module, "%s", std::vformat(fmt, std::make_format_args(std::forward<Args>(args)...)).c_str()); }
-    template <typename... Args> auto misc(std::string_view fmt, Args&&... args)
-        { log_body_misc(module, "%s", std::vformat(fmt, std::make_format_args(std::forward<Args>(args)...)).c_str()); }
-    template <typename... Args> auto warning(std::string_view fmt, Args&&... args)
-        { log_body_warning(module, "%s", std::vformat(fmt, std::make_format_args(std::forward<Args>(args)...)).c_str()); }
+    template <typename... Args> auto constexpr fatal(std::format_string<Args...> fmt, Args&&... args)
+        { log_body_fatal(module, "%s", std::format(fmt, std::forward<Args>(args)...).c_str()); }
+    template <typename... Args> auto constexpr info(std::format_string<Args...> fmt, Args&&... args)
+        { log_body_info(module, "%s", std::format(fmt, std::forward<Args>(args)...).c_str()); }
+    template <typename... Args> auto constexpr misc(std::format_string<Args...> fmt, Args&&... args)
+        { log_body_misc(module, "%s", std::format(fmt, std::forward<Args>(args)...).c_str()); }
+    template <typename... Args> auto constexpr warning(std::format_string<Args...> fmt, Args&&... args)
+        { log_body_warning(module, "%s", std::format(fmt, std::forward<Args>(args)...).c_str()); }
 }
