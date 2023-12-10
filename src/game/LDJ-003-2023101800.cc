0c6507e8a8581825493280f8255f618cb8bcca8c
whowechina <whowechina@gmail.com>
2023-12-10 14:19:09 +0800

    Support for 31 Epolis (20231018)

---
+++ src/game/LDJ-003-2023101800.cc
@@ -0,0 +1,6 @@
+#include <cstddef>
+
+std::ptrdiff_t addr_hook_a        = 0x077D0E0;
+std::ptrdiff_t addr_hook_b        = 0x07E6DA0;
+std::ptrdiff_t addr_textures      = 0x76D1328;
+std::ptrdiff_t addr_device_offset = 0x00000E0;
