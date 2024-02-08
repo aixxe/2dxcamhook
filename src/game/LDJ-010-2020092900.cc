88ee1a72046f4543ebe82104dd613cddcae05a62
aixxe <me@aixxe.net>
2024-02-08 19:47:14 +0000

    Add support for HEROIC VERSE 2020092900 (010)

---
+++ src/game/LDJ-010-2020092900.cc
@@ -0,0 +1,6 @@
+#include <cstddef>
+
+std::ptrdiff_t addr_hook_a        = 0x06B89E0;
+std::ptrdiff_t addr_hook_b        = 0x071C950;
+std::ptrdiff_t addr_textures      = 0x4FD08B8;
+std::ptrdiff_t addr_device_offset = 0x00000D0;
