907850745153aa2079ca9590202119db3a46ce5a
aixxe <me@aixxe.net>
2024-02-08 19:55:14 +0000

    Add support for CastHour 2022082400 (010)

---
+++ src/game/LDJ-010-2022082400.cc
@@ -0,0 +1,6 @@
+#include <cstddef>
+
+std::ptrdiff_t addr_hook_a        = 0x0523D60;
+std::ptrdiff_t addr_hook_b        = 0x058E440;
+std::ptrdiff_t addr_textures      = 0x6E9B648;
+std::ptrdiff_t addr_device_offset = 0x00000D8;
