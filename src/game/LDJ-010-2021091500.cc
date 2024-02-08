d1aa57dbd7f51bd2c1a8c803ce2fdb6099d71328
aixxe <me@aixxe.net>
2024-02-08 19:52:27 +0000

    Add support for BISTROVER 2021091500 (010)

---
+++ src/game/LDJ-010-2021091500.cc
@@ -0,0 +1,6 @@
+#include <cstddef>
+
+std::ptrdiff_t addr_hook_a        = 0x081C800;
+std::ptrdiff_t addr_hook_b        = 0x08868C0;
+std::ptrdiff_t addr_textures      = 0x5CDE4E8;
+std::ptrdiff_t addr_device_offset = 0x00000D8;
