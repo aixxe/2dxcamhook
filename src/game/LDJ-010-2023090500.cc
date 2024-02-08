7c3038c5bd2fb31caf35f0a112985e4270b112d6
aixxe <me@aixxe.net>
2024-02-08 19:58:47 +0000

    Add support for RESIDENT 2023090500 (010)

---
+++ src/game/LDJ-010-2023090500.cc
@@ -0,0 +1,6 @@
+#include <cstddef>
+
+std::ptrdiff_t addr_hook_a        = 0x064E7B0;
+std::ptrdiff_t addr_hook_b        = 0x06B71B0;
+std::ptrdiff_t addr_textures      = 0x7106E98;
+std::ptrdiff_t addr_device_offset = 0x00000E0;
